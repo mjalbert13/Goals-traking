fd51f9bf69f47179022f133f44d6396b72d317fa
Matt Jalbert <mjalbert13@gmail.com>
2022-05-14 11:45:41 -0400

    Added error handleing

--- backend/controlers/goalsControler.js
+++ backend/controlers/goalsControler.js
@@ -1,27 +1,34 @@
+const asyncHandler = require('express-async-handler')
+
 // Get Goals
 //  Route  GET /api/goals
 // Access Private
-const getGoals = (req, res)=>{
+const getGoals =asyncHandler( async(req, res)=>{
     res.status(200).json({message:'Get Goals'})
-}
+})
 // Set Goals
 //  Route  POST /api/goals
 // Access Private
-const postGoals = (req, res)=>{
+const postGoals = asyncHandler( async(req, res)=>{
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add text feild')
+    }
+
     res.status(200).json({message:'Set Goals'})
-}
+})
 // Update Goals
 //  Route  PUT /api/goals/:id
 // Access Private
-const putGoals = (req, res)=>{
+const putGoals =asyncHandler( async(req, res)=>{
     res.status(200).json({message:`Update Goal ${req.params.id}`})
-}
+})
 // Delete Goals
 //  Route  Delete /api/goals/:id
 // Access Private
-const deleteGoals = (req, res)=>{
+const deleteGoals = asyncHandler( async(req, res)=>{
     res.status(200).json({message:`Delete Goal ${req.params.id}`})
-}
+})
 
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const{ errorHandler} =require('./middleware/errorMiddleware')
 
 const app = express()
 
-app.use('/api/goals', require('./routs/goalsRouts'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
+
+app.use('/api/goals', require('./routs/goalsRouts'))
+app.use(errorHandler)
 app.listen(port, () => console.log(`Server Started on port ${port}`))
